a01004a5a5f9fc0d53410b89a93725ece227295b
Eistiak Ahmed <eistiakahmedmeraj@gmail.com>
2025-11-13 17:39:50 +0600

    Implement SDK firebase token and protect route

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const cors = require('cors');
+const admin = require('firebase-admin');
+const serviceAccount = require('./firebase-adminKey.json');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB URL
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// Verify Firebase Token Middleware
+const verifyToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.status(401).send({
+      message: 'Unauthorized access. Token not found!',
+    });
+  }
+
+  const token = authorization.split(' ')[1];
+  try {
+    const decodedUser = await admin.auth().verifyIdToken(token);
+    req.user = decodedUser;
+    next();
+  } catch (error) {
+    res.status(401).send({
+      message: 'Unauthorized access. Invalid token.',
+    });
+  }
+};
+
+// MongoDB URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@fureverlydb.o2jukph.mongodb.net/?appName=fureverlyDB`;
 
 const client = new MongoClient(uri, {
@@ -21,15 +50,14 @@ const client = new MongoClient(uri, {
   },
 });
 
-// Default route
+
 app.get('/', (req, res) => {
-  res.send(' Fureverly Server is running...');
+  res.send('Fureverly Server is running...');
 });
 
 async function run() {
   try {
     // await client.connect();
-
     const db = client.db('fureverlyDB');
     const petCollection = db.collection('petCollection');
     const orderCollection = db.collection('orderCollection');
@@ -44,111 +72,103 @@ async function run() {
       res.send(result);
     });
 
-    //  All products
+    // All products
     app.get('/product', async (req, res) => {
       const result = await petCollection.find().toArray();
       res.send(result);
     });
 
-    //  Single product by ID
+    // Single product by ID
     app.get('/product/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const result = await petCollection.findOne(filter);
+      const result = await petCollection.findOne({ _id: new ObjectId(id) });
       res.send(result);
     });
 
-    app.post('/product', async (req, res) => {
+    // Add new product (protected)
+    app.post('/product', verifyToken, async (req, res) => {
       const newProduct = req.body;
       const result = await petCollection.insertOne(newProduct);
       res.send(result);
     });
 
-    //  Category wise filter
+    // Update product by ID (protected)
+    app.put('/product/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const result = await petCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: data }
+      );
+      res.send(result);
+    });
+
+    // Delete product by ID (protected)
+    app.delete('/product/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const result = await petCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    // Category filter
     app.get('/products', async (req, res) => {
       const category = req.query.category;
-      let query = {};
-
-      if (category) {
-        query = { category: category };
-      }
+      const query = category ? { category } : {};
       const result = await petCollection.find(query).toArray();
       res.send(result);
     });
- 
-    // Update Method
-    app.put('/product/:id', async (req, res) => {
-      const id = req.params.id;
-      const data = req.body;
-      const filter = { _id: new ObjectId(id) };
-      const update = {
-        $set: data,
-      };
 
-      const result = await petCollection.updateOne(filter, update);
-      res.send(result);
-    });
-
-    app.delete('/product/:id', async(req,res) => { 
-      const id = req.params.id
-      const result = await petCollection.deleteOne({_id: new ObjectId(id)})
-      res.send(result)
-    })
-
-    // Search Method
+    // Search by category
     app.get('/filterCategory', async (req, res) => {
       const search_text = req.query.search;
-      const result = await petCollection
-        .find({ category: { $regex: search_text, $options: 'i' } })
-        .toArray();
+      const query = search_text
+        ? { category: { $regex: search_text, $options: 'i' } }
+        : {};
+      const result = await petCollection.find(query).toArray();
       res.send(result);
     });
 
+    // Search by name
     app.get('/searchName', async (req, res) => {
       const search_text = req.query.search;
-      const result = await petCollection
-        .find({ name: { $regex: search_text, $options: 'i' } })
-        .toArray();
+      const query = search_text
+        ? { name: { $regex: search_text, $options: 'i' } }
+        : {};
+      const result = await petCollection.find(query).toArray();
       res.send(result);
     });
 
+    // Get listings by logged-in user (protected)
+    app.get('/myListing', verifyToken, async (req, res) => {
+      const email = req.user.email;
+      const result = await petCollection.find({ email }).toArray();
+      res.send(result);
+    });
 
-
-     
-    // Find by Email
-    app.get('/myListing', async (req, res) => {
+    // Orders (protected)
+    app.get('/orders', verifyToken, async (req, res) => {
       const email = req.query.email;
-      const result = await petCollection.find({ email: email }).toArray();
+      const query = email ? { email } : {};
+      const result = await orderCollection.find(query).toArray();
       res.send(result);
     });
 
-    // Order 
-    app.get('/orders', async (req, res) => {
-      const result = await orderCollection.find().toArray();
-      res.send(result);
-    });
-
-    app.get('/orders', async(req,res) => { 
-      const email = req.query.email
-      const result = await orderCollection.find({email: email}).toArray()
-      res.send(result)
-    })
-
-    // Order Post 
-    app.post('/orders', async (req, res) => {
+    // Add order (protected)
+    app.post('/orders', verifyToken, async (req, res) => {
       const newOrder = req.body;
+      newOrder.email = req.user.email;
       const result = await orderCollection.insertOne(newOrder);
       res.send(result);
     });
 
-    // await client.db('admin').command({ ping: 1 });
-    console.log(' MongoDB Connected Successfully!');
+    console.log('MongoDB Connected Successfully!');
   } finally {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(` Fureverly ServerDB is running on port: ${port}`);
+  console.log(`Fureverly Server is running on port: ${port}`);
 });
